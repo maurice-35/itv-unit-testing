1dc4a958f122b3c5b09e8cde1373cf6f949de15a
mauricekollewe <kollewe.35@gmail.com>
2022-08-17 14:20:05 +0100

    testing title and alt

--- src/tests/task-01-queries.test.js
+++ src/tests/task-01-queries.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import InfoTile from "../components/InfoTile";
+
 
 const infoTileMock = {
   name: "Llama",
@@ -13,9 +14,15 @@ const infoTileMock = {
 const renderComponent = () => render(<InfoTile {...infoTileMock} />);
 
 describe("given the InfoTile component is rendered", () => {
-  it("then should contain a title", () => {});
+  it("then should contain a title", () => {
+    renderComponent()
+    expect(screen.getByRole("heading", { name: infoTileMock.latinName })).toBeInTheDocument()
+  });
 
-  it("then should contain an image with the expect alt text and source", () => {});
+  it("then should contain an image with the expect alt text and source", () => {
+    renderComponent()
+    expect(screen.getByRole("img", { name: infoTileMock.alt })).toBeInTheDocumet;
+  });
 
   it("then should contain all 4 bullet points with expected text in the correct order", () => {});
 });
